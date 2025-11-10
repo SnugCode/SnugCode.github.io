2b79ae69915621aa733002ccfa4f160e1d9fba9a
SnugCode <snadith2003@gmail.com>
2025-11-11 02:48:05 +1100

    Final Refactor

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Hello
+The following is a log on all the changes I've made for my Developer Profile.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-js;
+const siteTitle = "SnugCode Portfolio";
+const currentYear = new Date().getFullYear();
+
+function init() {
+    console.log("Welcome to " + siteTitle + " - " + currentYear);
+}
